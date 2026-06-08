52d67db25d182fe0b27cd29ab770d4ab8d573193
isa._. <isacinimartinhao@gmail.com>
2026-06-08 11:19:11 -0300

    Add files via upload

--- logica-js-projeto_inicial (2)/logica-js-aula_JogoDoNumeroOculto/app.js
+++ logica-js-projeto_inicial (2)/logica-js-aula_JogoDoNumeroOculto/app.js
@@ -1,7 +1,10 @@
 alert('Bem vindo ao jogo do número secreto ^_^');
-let numeroSecreto = 5;
+let numeroSecreto = 8;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('parabéns! Você descobriu o número secreto ^^ (5)');
+    alert(`parabéns! Você descobriu o número secreto ^^ (${numeroSecreto})`);
+} else {
+    alert('Que pena! Você errou:<');
 }
